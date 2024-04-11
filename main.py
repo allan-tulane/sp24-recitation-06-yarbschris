7c200275fe1315e90ee8135c567400c77d6fd740
yarbschris <123770805+yarbschris@users.noreply.github.com>
2024-04-11 02:55:50 +0000

    Final

--- main.py
+++ main.py
@@ -1,34 +1,35 @@
 def fib_recursive(n, counts):
-    """
-    Return the nth Fibonacci number. 
-    counts is a list of n+1 elements, where counts[i] is incremented
-    each time fib_recursive(i, counts) is called.
-    """    
-
-    if n == 0:
+  """
+  Return the nth Fibonacci number. 
+  counts is a list of n+1 elements, where counts[i] is incremented
+  each time fib_recursive(i, counts) is called.
+  """    
+  if n == 0:
       return 0
-
-    if n == 1:
+  if n == 1:
       return 1
 
-    
-    counts[n] += 1
-    return fib_recursive(n-1, counts) + fib_recursive(n-2, counts)
-    
+  counts[n] += 1
 
-    
+
+  return fib_recursive(n-1, counts) + fib_recursive(n-2, counts)
+
+
+
 def fib_top_down(n, fibs):
-    if fibs[n] != -1:
+
+  if fibs[n] != -1:
       return fibs[n]
-    if n == 0:
+
+
+  if n == 0:
       fibs[0] = 0
-    elif n == 1:
+  elif n == 1:
       fibs[1] = 1
-    else:
+  else:
       fibs[n] = fib_top_down(n-1, fibs) + fib_top_down(n-2, fibs)
 
-    return fibs[n] 
-
+  return fibs[n] 
 
 def fib_bottom_up(n):
   if n <= 1:
@@ -41,7 +42,3 @@ def fib_bottom_up(n):
       fibs[i] = fibs[i - 1] + fibs[i - 2]
 
   return fibs[n]
-
-
-
-
